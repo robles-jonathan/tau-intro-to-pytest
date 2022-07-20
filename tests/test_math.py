4e2ae3ecbea1e7e95e60e6d2cb0d46a8b6721341
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-20 15:35:54 -0500

    ch 4 Parameterized Test Cases

--- tests/test_math.py
+++ tests/test_math.py
@@ -7,6 +7,7 @@ Their purpose is to show how to use the pytest framework by example.
 #   Imports
 #------------------------------------------------------------------------------------------------
 
+from math import prod
 import pytest
 
 
@@ -54,6 +55,7 @@ def test_one_plus_two():
     c = 3
     assert a + b == c
     
+    
     """
     1. One Python test module can contain more than one test case function.
         true
@@ -80,6 +82,8 @@ def test_divide_by_zero():
         num = 1 / 0
     
     assert 'division by zero' in str(e.value)
+    
+    
     """
     1. What is the purpose of "pytest.raises"?
     To verify that the code under test raises expected exceptions.
@@ -96,3 +100,62 @@ def test_divide_by_zero():
     5. How can "pytest.raises" store the raised exception object for further assertions?
     Add an "as" clause to the end of the "with" statement to store the exception into a variable.
     """
+    
+    # Multiplication test ideas
+    
+    # two positive integers
+    # identity: multiply any number by 1
+    # zero: multiply any number by 0
+    # positive by a negative
+    # negative by a negative
+    # multiply floats
+    """
+    
+    def test_multiply_two_positive_ints():
+        assert 2 * 3 == 6
+        
+        
+    def test_multiply_identity():
+        assert 1 * 99 == 99
+        
+        
+    def test_multiply_zero():
+        assert 0 * 100 == 0 
+        
+        
+    # DRY Principle: Don't Repeat Yourself!
+    """
+    
+#------------------------------------------------------------------------------------------------
+#   A parametrized test function
+#------------------------------------------------------------------------------------------------
+
+products = [
+    (2, 3, 6),              # positive integers
+    (1, 99, 99),            # identity
+    (0, 99, 0),             # zero
+    (3, -4, -12),           # positive by negative
+    (-5, -5, 25),           # negative by negative
+    (2.5, 6.7, 16.75)       # floats
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b , product):
+    assert a * b == product 
+    
+"""
+1. What is an "equivalence class" for test case inputs?
+    A unique kind of input that yields a unique kind of output.
+    
+2. What is the name of the pytest decorator for parametrized inputs?
+    @pytest.mark.parametrize
+    
+3. What is the data format for passing pytest parameters?
+    a list of parameter value tuples
+    
+4. pytest parameters may be strings as well as numbers.
+    true
+    
+5. pytest parameters cannot be read from external files.
+    false
+"""
