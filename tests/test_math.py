1cac17d45fa3d20bd335d40f4641adddbf24ab27
Jonathan Robles <jonathan.robles287@gmail.com>
2022-07-21 09:28:56 -0500

    ch8 Filtering tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -48,7 +48,7 @@ def test_one_plus_two():
     c = 0
     assert a + b == c
 """
-
+@pytest.mark.math
 def test_one_plus_two():
     a = 1
     b = 2
@@ -77,6 +77,7 @@ def test_one_plus_two():
 #   A test function that verifies an exception
 #------------------------------------------------------------------------------------------------
 
+@pytest.mark.math
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
@@ -139,6 +140,7 @@ products = [
     (2.5, 6.7, 16.75)       # floats
 ]
 
+@pytest.mark.math
 @pytest.mark.parametrize('a, b, product', products)
 def test_multiplication(a, b , product):
     assert a * b == product 
